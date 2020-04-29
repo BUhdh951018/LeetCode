14e0e75b905038441030de4c5bd9e834352ec328
donghanghe <hdhssl1015>
2020-04-29 14:02:36 -0400

    No33

---
+++ src/No33/BinarySearch2_33.java
@@ -0,0 +1,29 @@
+package No33;
+
+public class BinarySearch2_33 {
+    public int search(int[] nums, int target) {
+        int lo = 0, hi = nums.length - 1, mid = 0;
+        while (lo <= hi) {
+            mid = lo + (hi - lo) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[mid] >= nums[lo]) {
+                if (target >= nums[lo] && target < nums[mid]) {
+                    hi = mid - 1;
+                } else {
+                    lo = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[hi]) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ src/No33/BinarySearch_33.java
@@ -0,0 +1,31 @@
+package No33;
+
+public class BinarySearch_33 {
+    public int search(int[] nums, int target) {
+        int lo = 0, hi = nums.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (target >= nums[0]) {
+                if (nums[mid] < nums[0]) {
+                    nums[mid] = Integer.MAX_VALUE;
+                }
+            } else {
+                if (nums[mid] >= nums[0]) {
+                    nums[mid] = Integer.MIN_VALUE;
+                }
+            }
+
+            if (nums[mid] < target) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
