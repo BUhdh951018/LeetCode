2b5d335eb75a8ea8f3c5c75b9291a08e9bcd33a8
donghanghe <hdhssl1015>
2020-06-14 13:56:24 -0400

    No50

---
+++ src/No50/Recursive_50.java
@@ -0,0 +1,17 @@
+package No50;
+
+public class Recursive_50 {
+    public double myPow(double x, int n) {
+        long N = n;
+        return N >= 0 ? quick(x, N) : 1.0 / quick(x, -N);
+    }
+
+    public double quick(double x, long N) {
+        if (N == 0)
+            return 1;
+
+        double y = quick(x, N / 2);
+
+        return N % 2 == 0 ? y * y : y * y * x;
+    }
+}
